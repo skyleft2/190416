5bc1640f3498aab2c88a857b36af2dd63478c410
skyleft2 <skyleft7506@gmail.com>
2019-04-16 11:28:25 +0900

    test02

---
+++ src/main/java/com/example/spring_boot_190415/SpringBoot190415Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_190415;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot190415Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot190415Application.class, args);
+	}
+
+}
